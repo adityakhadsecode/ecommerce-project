aa74ae3332342f59933e9723c93a16bb267a58cc
Aditya Khadse <adityakhadse.dev@gmail.com>
2026-03-31 16:59:54 +0530

    feat: integrate cart state management in App and pass cart data to HomePage and CheckoutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { HomePage } from "./Pages/HomePage";
 import { CheckoutPage } from "./Pages/CheckoutPage";
 import { OrdersPage } from "./Pages/OrdersPage";
@@ -6,10 +8,17 @@ import { TrackingPage } from "./Pages/TrackingPage";
 import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,17 +4,12 @@ import "./HomePage.css";
 import { Header } from "../Components/Header";
 import { products } from "../data/products.js";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
 
