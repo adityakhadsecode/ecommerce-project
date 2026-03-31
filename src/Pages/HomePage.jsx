8895e3d427e11e2f3f996354805cf5de8f9cab36
Aditya Khadse <adityakhadse.dev@gmail.com>
2026-03-31 16:46:38 +0530

    feat: update Header component to display total cart quantity and fetch cart items in HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,27 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import "./HomePage.css";
 import { Header } from "../Components/Header";
 import { products } from "../data/products.js";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+
+    axios.get("http://localhost:3000/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Home</title>
-      <Header />
+      <Header cart={cart} />
 
       <div className="home-page">
         <div className="products-grid">
