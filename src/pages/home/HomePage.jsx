236a92ec9b41c3ef434b3249b7d93696e5207559
Aditya Khadse <adityakhadse.dev@gmail.com>
2026-04-02 16:57:44 +0530

    feat: implement Product component with add to cart functionality and integrate loadCart method

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Header } from '../../components/Header';
-import { ProductsGrid } from './ProductsGrid';
-import './HomePage.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Header } from "../../components/Header";
+import { ProductsGrid } from "./ProductsGrid";
+import "./HomePage.css";
 
-export function HomePage({ cart }) {
+export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get('/api/products');
+      const response = await axios.get("/api/products");
       setProducts(response.data);
     };
 
@@ -23,7 +23,7 @@ export function HomePage({ cart }) {
       <Header cart={cart} />
 
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
