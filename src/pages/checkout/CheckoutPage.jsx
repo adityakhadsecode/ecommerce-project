0935034e5c4c592d6c54bb248c77fa350aea66af
Aditya Khadse <adityakhadse.dev@gmail.com>
2026-04-02 17:17:51 +0530

    feat: enhance checkout flow by integrating loadCart method in CheckoutPage, DeliveryOptions, and OrderSummary components

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,27 +1,27 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { OrderSummary } from './OrderSummary';
-import { PaymentSummary } from './PaymentSummary';
-import './checkout-header.css';
-import './CheckoutPage.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { OrderSummary } from "./OrderSummary";
+import { PaymentSummary } from "./PaymentSummary";
+import "./checkout-header.css";
+import "./CheckoutPage.css";
 
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart, loadCart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
       let response = await axios.get(
-        '/api/delivery-options?expand=estimatedDeliveryTime'
+        "/api/delivery-options?expand=estimatedDeliveryTime",
       );
       setDeliveryOptions(response.data);
 
-      response = await axios.get('/api/payment-summary');
+      response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
 
   return (
     <>
@@ -37,8 +37,11 @@ export function CheckoutPage({ cart }) {
           </div>
 
           <div className="checkout-header-middle-section">
-            Checkout (<a className="return-to-home-link"
-              href="/">3 items</a>)
+            Checkout (
+            <a className="return-to-home-link" href="/">
+              3 items
+            </a>
+            )
           </div>
 
           <div className="checkout-header-right-section">
@@ -51,7 +54,11 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
 
           <PaymentSummary paymentSummary={paymentSummary} />
         </div>
